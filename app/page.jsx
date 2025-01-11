5543894459b76d019c17d86fc07629c21f4a3db1
hafida faouzi <hafida.faouzi@inventiv-it.fr>
2025-01-11 13:08:00 +0100

    adding section to home page and all needed components

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,18 @@
-
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        PromptoBilia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+      {/* Feed Component */}
+    </section>
+  );
+};
 
 export default Home;
